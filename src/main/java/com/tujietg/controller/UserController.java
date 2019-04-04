734a7588dcdc8cf92ae2bb4ec53fdb403b1ae9e0
tujietg <tujietg@gmail.com>
2019-04-04 11:19:07 +0800

    修复bug，增加登陆才能访问的注解，和url

--- src/main/java/com/tujietg/controller/UserController.java
+++ src/main/java/com/tujietg/controller/UserController.java
@@ -1,15 +1,16 @@
 package com.tujietg.controller;
 
 import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authz.annotation.RequiresAuthentication;
 import org.apache.shiro.authz.annotation.RequiresRoles;
 import org.apache.shiro.subject.Subject;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
 import com.tujietg.Exception.UnauthorizedException;
 import com.tujietg.model.ResponseData;
 import com.tujietg.pojo.UserBean;
@@ -52,7 +53,7 @@ public class UserController {
 	}
 
 	/**
-	 * 需要登陆才能访问的接口
+	 * 登陆与否访问的接口数据不同
 	 * 
 	 * @return
 	 */
@@ -67,7 +68,18 @@ public class UserController {
 	}
 
 	/**
-	 * 游客也可以访问的接口
+	 * 登陆才能访问的接口
+	 * 
+	 * @return
+	 */
+	@RequestMapping("/require_auth")
+	@RequiresAuthentication
+	public ResponseData requireAuth() {
+		return new ResponseData(200, "You are authenticated", null);
+	}
+
+	/**
+	 * 游客登陆访问的接口
 	 */
 	@RequestMapping(value = "/direct")
 	public ResponseData direct() {
