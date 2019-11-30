99200d11bee40c1143fbe997a45968317133d8d2
tujietg <tujietg@gmail.com>
2019-11-30 14:32:53 +0800

    更新版本

---
+++ src/test/java/com/macro/mall/malltiny01/MallTiny01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.macro.mall.malltiny01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallTiny01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
