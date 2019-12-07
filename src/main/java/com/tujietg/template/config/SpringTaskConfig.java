4a8b65d639396e4d52f6b0983f5988258bb48735
tujietg <tujietg@gmail.com>
2019-12-07 10:04:16 +0800

    增加spring task

---
+++ src/main/java/com/tujietg/template/config/SpringTaskConfig.java
@@ -0,0 +1,13 @@
+package com.tujietg.template.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ * @author tujietg
+ * @date 2019/12/7 10:03 AM
+ */
+@Configuration
+@EnableScheduling
+public class SpringTaskConfig {
+}
